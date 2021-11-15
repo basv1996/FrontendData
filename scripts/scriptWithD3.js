23993d3ba14ce6f950f433ff630aff05d7568ac7
basv1996 <bas.vugts@gmail.com>
2021-11-15 11:24:18 +0100

    starting with API

---
+++ scripts/scriptWithD3.js
@@ -0,0 +1,5 @@
+// https://api.punkapi.com/v2/ 
+
+const margin = {top: 40, bottom: 10, left: 120, right: 20};
+const width = 800 - margin.left - margin.right;
+const height = 600 - margin.top - margin.bottom;
