f77aecd8802d5c8572a770714ad278da0fcd100c
basv1996 <bas.vugts@gmail.com>
2021-11-15 13:35:26 +0100

    filter works

--- scripts/scriptWithD3.js
+++ scripts/scriptWithD3.js
@@ -9,24 +9,24 @@ const beer_height = 50;
  // Creates sources <svg> element
  const svg = d3.select('body').append('svg')
  .attr('width', width+margin.left+margin.right)
- .attr('height', height+margin.top+margin.bottom);
+ .attr('height', height+margin.top+margin.bottom)
 
 // Group used to enforce margin
   const g = svg.append('g')
-              .attr('transform', `translate(${margin.left},${margin.top})`);
+              .attr('transform', `translate(${margin.left},${margin.top})`)
 
 d3.json('https://api.punkapi.com/v2/beers?page=2&per_page=10').then((json) => {
 data = json
-console.log(json)
-console.log(json.Object)
-console.log(json[0].name)
-console.log(json.map((obj) => {
-    return obj.name
-}))
+// console.log(json)
+// console.log(json.Object)
+// console.log(json[0].name)
+// console.log(json.map((obj) => {
+//     return obj.name
+// }))
 
-console.log(json.map((obj) => {
-    return obj.tagline
-}))
+// console.log(json.map((obj) => {
+//     return obj.tagline
+// }))
 
 console.log(json.map((obj) => {
     return obj.abv
@@ -56,23 +56,29 @@ function updateMe(new_data) {
     (exit) => exit.remove()
     );
 
-    rect
+    rect.transition()
     .attr('height', beer_height)
     .attr('width', (d) => d.abv * 7)
-    .attr('y', (d, i) => i*(beer_height+5));
+    .attr('y', (d, i) => i*(beer_height+5))
 
     rect.select('title')
     .text((d) => 
     d.name)
   }
 
-  d3.select('.more-than-x-percentage').on('change'), function() {
-      // This will be triggered when the user selects or unselects the checkbox
-  const checked = d3.select(this).property('checked');
-  if (checked === true) {
-    // Checkbox was just checked
-    console.log("checked")
+  d3.select('#alcoholPercentage').on('change', 
+  function() {
+     
+             const checked = d3.select(this).property('checked')  // als de gebruiker op de checkbox klikt
+                if (checked == true) { // als er op de checkbox geklikt is en deze aangevinkt is
+                    const high_percentage = data.filter((d) => 
+                    d.abv >= 5)
+
+                    updateMe(high_percentage)
+                } else {
+                     updateMe(data)
   }
+});
 
 
 
