42941d11e2a2a4f9e80bf77806f488c72790cad2
basv1996 <bas.vugts@gmail.com>
2021-11-18 12:07:56 +0100

    added extra code comments

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,5 @@
 // https://api.punkapi.com/v2/ 
 
-// https://pokeapi.co/api/v2/pokemon?limit=100&offset=0
-
 const margin = {top: 40, bottom: 140, left: 150, right: 20};
 const width = 800 - margin.left - margin.right;
 const height = 600 - margin.top - margin.bottom;
@@ -17,42 +15,35 @@ const height = 600 - margin.top - margin.bottom;
               // transformeer de getekende chart en verplaats hem x stappen van links en x stappen van de top (in dit geval 150 en 40)
 
 const xScale = d3.scaleBand()
+.domain([0,10])
 .range([ 0, width ])
-.domain([0, width])
-.padding(0.2); 
+.padding(0.2)
 
-
-//maak een scale met een domein van 0 tot 10 (10 stappen op een range van ...) en een range van 0 tot 100. 
-//In dit geval zou een range van 5 een output geven van 50
+//maak een scale met een range van 0 tot 100. 
 
 
 const yScale = d3.scaleLinear()
-//.domain([0, height])
-.range([ height, 0])
+.domain([0,10])
+.range([ height, 0]) //van height naar 0 zodat op de y-as de hoogte bovenaan staat
 
 
-const xaxis = d3.axisBottom().scale(xScale) // De cijfers worden aan de bovenkant van de x-as geplaatst
+const xaxis = d3.axisBottom().scale(xScale) // De data wordt aan de onderkant van de x-as geplaatst
 const drawXaxis = g.append('g').attr("class", "x-as") // teken een x as
 
-const yaxis = d3.axisLeft().scale(yScale)
+const yaxis = d3.axisLeft().scale(yScale) // De data wordt aan de linkerkant van de as geplaatst
 const drawYaxis = g.append('g').attr("class", "y-as") //teken een y as
 
 d3.json('https://api.punkapi.com/v2/beers?page=1&per_page=11').then((json) => {
+  // Verzendt http-request naar de opgegeven url om .json-bestand of gegevens te laden
 data = json
 
 updateMe(data)
 }) 
 
 function updateMe(new_data) {
-
      // update de schalen
-  yScale
-  .domain([0, d3.max(new_data, (d) => 
-    d.abv)])
-
-  xScale
-  .domain(new_data.map((d) =>
-   d.name))
+  yScale.domain([0, d3.max(new_data, (d) => d.abv)])
+  xScale.domain(new_data.map((d) => d.name))
 
    //maak de assen
   drawXaxis
@@ -69,13 +60,12 @@ function updateMe(new_data) {
   .call(yaxis)
 
     // DATA JOIN
-    //const makeItAlink = rect
   const rect = g
   .selectAll('rect')
   .data(new_data)
   .join(
      // ENTER 
-     // new elements
+     // nieuwe elementen
        (enter) => {
          const rect_enter = 
          enter.append('rect').attr('x', 0)
@@ -83,10 +73,10 @@ function updateMe(new_data) {
           return rect_enter
     },
     // UPDATE
-    // update existing elements
+    // Update de bestaande elementen
     (update) => update,
     // EXIT
-    // elements that aren't associated with data
+    // de elementen die die niet geassocieert zijn met de data
     (exit) => exit.remove()
     )
 
@@ -96,22 +86,19 @@ function updateMe(new_data) {
     .duration(1000)
     .attr("x", (d) => xScale(d.name))
     .attr("y", (d) => yScale(d.abv))
-    .attr("width", xScale.bandwidth())
-    .attr("height", (d) => height -yScale(d.abv))
+    .attr("width", xScale.bandwidth()) //bandwidth wordt gebruik om de bandbreedte van de X schaal te vinden
+    .attr("height", (d) => height -yScale(d.abv)) // de hoogte van een rectangle(bar) is de hoogte per object min de y schaal van het alcohol percentage
     
     
     rect.select('title')
     .text((d) => 
-   d.name + ": " + d.abv)
+   d.name + ": " + d.abv) //geef de rectangles een title van de naam van het biertje en het alcohol permilage
    
-   //geef de rectangles een title van de naam van het biertje en het alcohol permilage
+   rect.append('a').attr("href", (d) => d.image_url).text((d) => d.name)// voeg een a tag toe met een href met de gegevens van image_url en geef de tekst de naam
 
    rect.on('click', (e) =>
-   window.open(e.target.childNodes[1].getAttribute('href'))
+   window.open(e.target.childNodes[1].getAttribute('href')) // Als er geklikt wordt op de bar open dan de link binnen in de rectangle
    )
-
-   rect.append('a').attr("href", (d) => d.image_url).text((d) => d.name)
-
   }
 
   d3.select('#alcoholPercentage').on('change', 
